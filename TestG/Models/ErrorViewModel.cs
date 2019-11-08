07ca8ee7130ee934bca900e00f63412f71e7a704
lololipop41 <56724086+lololipop41@users.noreply.github.com>
2019-11-08 15:34:37 +0800

    Add project files.

---
+++ TestG/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestG.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
